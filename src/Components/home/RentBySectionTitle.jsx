89c5facfd4aa5c2d1a270b56aa0c03f9ef490a6f
kwamedanso <dansokwame96@gmail.com>
2024-02-13 11:06:41 -0600

    Enable Component receive props from parent

--- src/Components/home/RentBySectionTitle.jsx
+++ src/Components/home/RentBySectionTitle.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs';
 
 
-export default function RentBySctionTitle() {
+export default function RentBySctionTitle({ variant }) {
     return (
         <div className="rent__by__title flex jc-space-between">
-            <p className="fs-400 fw-semi-bold">Rent by Brands</p>
+            <p className="fs-400 fw-semi-bold">Rent by {variant}</p>
             <Link to='/vehicles' className='button flex-container' data-type='inverted' data-attr='block-button'><span className='padding-right-1'>View all</span> <BsArrowRight /></Link>
         </div>
     )
