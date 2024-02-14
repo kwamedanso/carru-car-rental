d704fc2f890a1f48e611c842ff30688e62b027b3
kwamedanso <dansokwame96@gmail.com>
2024-02-14 02:12:05 -0600

    Add padding-block to increase spacing

--- src/Components/home/Testimonial.jsx
+++ src/Components/home/Testimonial.jsx
@@ -6,7 +6,7 @@ export default function Testimonial() {
     return (
         <>
             <section id="testimonial">
-                <div className="testimonial__wrapper section-margin">
+                <div className="testimonial__wrapper section-margin padding-block-600">
                     <SectionTitle header={'Trusted by Thousands of happy customers'} description={'Renting a luxury car has never been easier. Our streamlined process makes it simple for you to book and convirm your vehicle of choice online'} />
                 </div>
             </section>
